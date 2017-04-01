ddb60fa04aa5df55ab605a6c83e17898c6387e58
DESKTOP-5S5C5OR\Dave Trebas <dptrebas@gmail.com>
2017-04-01 07:54:37 -0700

    Added Error Modal Box and Source Code

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,40 @@
+// ErrorModal Component for the Weather app
+var React = require('react');
+
+// Make this a stateless functional component and only return one thing - Using fat arrows
+var ErrorModal = React.createClass( {
+
+	propType: {
+		title: React.PropTypes.string,
+		message: React.PropTypes.string.isRequired
+	},
+
+	getDefaultProps: function () {
+		return {
+			title: 'Error'
+		};
+	},
+
+	componentDidMount: function() {
+		var modal = new Foundation.Reveal($('#error-modal'));
+		modal.open();
+	},
+
+	render: function() {
+		var {title, message} = this.props;
+
+		return (
+			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
+				<h4>{title}</h4>
+				<p>{message}</p>
+				<p>
+					<button className="button hollow" data-close="">
+					 OK
+					</button>
+				</p>
+			</div>
+		);
+	}
+});
+
+module.exports = ErrorModal;
