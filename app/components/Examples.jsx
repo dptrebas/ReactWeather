1f7868ece98174f7483355386894d0b72b038693
DESKTOP-5S5C5OR\Dave Trebas <dptrebas@gmail.com>
2017-04-01 06:16:36 -0700

    Added foundation styles for Main and Example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,13 +1,17 @@
 // Examples Component for the Weather app
-
 var React = require('react');
+var {Link} = require('react-router');
 
 // Stateless Functional Component
 var Examples = (props) => {
 		return (
 			<div>
-				<h3>Examples</h3>
-				<p>Welcome to the examples page!</p>
+				<h1 className="text-center">Examples</h1>
+				<p>Here are a few example locations to try out</p>
+				<ol>
+					<li><Link to='/?location=Miami'>Miami</Link></li>
+					<li><Link to='/?location=Fresno'>Fresno</Link></li>
+				</ol>
 			</div>
 		);
 };
