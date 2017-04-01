dc2649ecab9136f44c0f62318345d50ab09dc786
DESKTOP-5S5C5OR\Dave Trebas <dptrebas@gmail.com>
2017-04-01 00:21:42 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000; 
+
+// Don't allow https
+app.use(function (req, res, next) {
+    if(req.header['x-forwarded-proto'] === 'http') {
+        next();
+    } else{
+        res.redirect('http://'+req.hostname+req.url);
+    }
+});
 
 // Use this
 app.use(express.static('public'));
 
 // Start our web server up
-app.listen(3000, function () {
-        console.log('Express server is up on port 3000')
+app.listen(PORT, function () {
+    console.log('Express server is up on port ' + PORT)
 });
 
