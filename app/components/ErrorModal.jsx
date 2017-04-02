ec9daaff89d292326242ee966f20ba3df848b803
DESKTOP-5S5C5OR\Dave Trebas <dptrebas@gmail.com>
2017-04-02 02:04:07 -0700

    Fixed Modal DOM Error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,5 +1,7 @@
 // ErrorModal Component for the Weather app
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 // Make this a stateless functional component and only return one thing - Using fat arrows
 var ErrorModal = React.createClass( {
@@ -16,14 +18,8 @@ var ErrorModal = React.createClass( {
 	},
 
 	componentDidMount: function() {
-		var modal = new Foundation.Reveal($('#error-modal'));
-		modal.open();
-	},
-
-	render: function() {
 		var {title, message} = this.props;
-
-		return (
+		var modalMarkup = (
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
 				<h4>{title}</h4>
 				<p>{message}</p>
@@ -33,6 +29,19 @@ var ErrorModal = React.createClass( {
 					</button>
 				</p>
 			</div>
+		);
+
+		// Use jQuery to update the DOM
+		var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+		$(ReactDOM.findDOMNode(this)).html($modal);
+
+		var modal = new Foundation.Reveal($('#error-modal'));
+		modal.open();
+	},
+
+	render: function() {
+		return (
+			<div></div>
 		);
 	}
 });
